87c1e3350d7490de76e3a8f9dd5001d101141b91
PupkinLangford <tahellman@gmail.com>
2020-11-13 21:15:18 -0500

    add hiscore to state, update with useEffect

--- src/App.js
+++ src/App.js
@@ -8,12 +8,18 @@ import Board from './Components/Board'
 function App() {
 
   const [score, setScore] = useState(0);
+  const [hiscore, setHiscore] = useState(0);
 
-  useEffect (() => console.log(score));
+  useEffect (() => {
+    if (score > hiscore) {
+      setHiscore(score);
+    }
+  }, [score, hiscore]);
 
   return (
     <div className="App">
       <div>{score}</div>
+      <div>{hiscore}</div>
       <Board handler={setScore}/>
     </div>
   );
